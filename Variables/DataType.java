847fad06c97d39ce7dc6064c555f2f74f61d8448
kawsarkabir <devkawsarkabir@gmail.com>
2023-11-29 17:23:23 +0600

    data type

---
+++ Variables/DataType.java
@@ -0,0 +1,13 @@
+public class DataType{
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        boolean isStudent = true;
+        char grade = 'A';
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+    }
+}
---
+++ Variables/Variable.java
@@ -0,0 +1,2 @@
+// A variable is a container (storage area) used to hold data. Each variable should be given a unique name (identifier).  
+
