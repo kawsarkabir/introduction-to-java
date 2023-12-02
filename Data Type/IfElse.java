5d7f4c7ed5b5584762cc75ed84a2f62dd7bbee15
kawsarkabir <devkawsarkabir@gmail.com>
2023-12-02 23:28:00 +0600

    if else structure

---
+++ Data Type/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is non-positive.");
+        }
+    }
+}
